11b308c86c7918d52fe8fd8583909ae6953fa84c
MikhailKuznetsov <qtd130@gmail.com>
2018-09-25 15:15:09 +0300

    design complete

---
+++ app/src/main/java/com/example/mikhail/exercise2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.mikhail.exercise2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
