a2161840107e7100205d1b021dbadecd2cffb397
MikhailKuznetsov <qtd130@gmail.com>
2018-09-25 17:43:41 +0300

    added button's functional

--- app/src/main/java/com/example/mikhail/exercise2/MainActivity.java
+++ app/src/main/java/com/example/mikhail/exercise2/MainActivity.java
@@ -1,13 +1,79 @@
 package com.example.mikhail.exercise2;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private String message;
+    private EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageButton vk = findViewById(R.id.vkButton);
+        vk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+             openWebPage("https://vk.com/timetosit");
+            }
+        });
+
+    ImageButton inst = findViewById(R.id.instaButton);
+        inst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+             openWebPage("https://www.instagram.com/foundpart");
+            }
+        });
+           ImageButton telegram = findViewById(R.id.telegramButton);
+        telegram.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+             openWebPage("https://t.me/chopyourbrain");
+            }
+        });
+           ImageButton github = findViewById(R.id.githubButton);
+        github.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+             openWebPage("https://github.com/chopyourbrain");
+            }
+        });
+        Button toEmail = findViewById(R.id.button);
+        editText = findViewById(R.id.editText);
+        toEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    message = editText.getText().toString();
+                    composeEmail("Hello",message); }
+                catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), R.string.noEmail, Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+    public void openWebPage(String url) {
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+    public void composeEmail(String subject, String message) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:qtd130@gmail.com"));
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
